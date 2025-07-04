586c74f80204f0618c1ecbfca741b902c73994e5
ezashi <soma3070@icloud.com>
2025-07-04 11:29:19 +0900

    feat: スタイルシートを追加
    - style.cssを新規作成し、基本設定や各要素のスタイルを定義しました。
    - admin.blade.phpとapp.blade.phpにstyle.cssのリンクを追加しました。

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>勤怠管理</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
   </head>
   <body>
     <header>
